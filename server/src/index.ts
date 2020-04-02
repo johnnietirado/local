82289539c65a46f6033ef51f808eefa7cfc61ca7
Johnnie Tirado <tiradoyamin@gmail.com>
2020-04-02 15:24:01 -0500

    video-0

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import express, { Request, Response } from 'express';
 
-import app from './server';
+const app = express();
 
 (async () => {
 
     const port = Number(process.env.PORT || 4000);
+
+    app.get('/', (req: Request, res: Response) => {
+        res.send('Hello World 🌎');
+    });
 
     // start the Express server
     app.listen(port, () => {
