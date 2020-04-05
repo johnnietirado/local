c76c764a170796a042a544074deae381f03a5356
Johnnie Tirado <tiradoyamin@gmail.com>
2020-04-05 13:54:27 -0500

    sync with video

--- server/src/models/users.model.ts
+++ server/src/models/users.model.ts
@@ -2,11 +2,13 @@ import mongoose, { Document } from 'mongoose';
 
 // Aquí empezamos a ver el poder de mongoose
 export interface IUser extends Document {
-    email: string
+    email: string,
+    password: string
 }
 
 const UserSchema = new mongoose.Schema<IUser>({
-    email: { unique: true, required: true, type: String }
+    email: { unique: true, required: true, type: String },
+    password: { required: true, type: String },
 });
 
 export default mongoose.models.Users || mongoose.model('users', UserSchema);
